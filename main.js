07014770910ab0fc0b6968142005de49c5ea55ee
AxlBravo <axlbravo365@gmail.com>
2023-04-06 00:51:32 -0600

    Prueba Técnica

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    var planetOption = $("#planet");
+  
+
+    $("#to-mars").click(function(event) {
+      
+      planetOption.attr("src", "./img/mars.png");
+    });
+  
+});
+
